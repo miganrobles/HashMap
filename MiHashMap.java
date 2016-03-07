d740b86cee9fe685556a406b2d88632b5cb2d71e
miguel <miganrobles@gmail.com>
2016-03-07 12:27:04 +0100

    Creada nueva clase MiHashMap con el contructor y sus atributos

---
+++ MiHashMap.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class MiHashMap here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MiHashMap
+{
+    // Almacena las claves de nuestro HashMap
+    private String[] claves;
+    // Almacena los elementos de nuestro HashMap
+    private int[] valores;
+   
+    /**
+     * Constructor for objects of class MiHashMap
+     */
+    public MiHashMap()
+    {
+        // initialise instance variables
+        claves = new String[0];
+        valores = new int[0];
+    }
+    
+}
